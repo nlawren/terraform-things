f35761918dbec6b721e3f1a041d3bfd829676a99
Nicholas Lawrence <nlawren@gmail.com>
2023-05-30 14:16:00 +1000

    Udemy MTC Terraform - Section 2.24

--- mtc-terraform-docker/main.tf
+++ mtc-terraform-docker/main.tf
@@ -13,11 +13,52 @@ resource "docker_image" "nodered_image" {
   name = "nodered/node-red:latest"
 }
 
+resource "random_string" "random" {
+  length = 4
+  special = false
+  upper = false
+}
+
+resource "random_string" "random2" {
+  length = 4
+  special = false
+  upper = false
+}
+
 resource "docker_container" "nodered_container" {
-  name  = "nodered"
+  name  = join("-",["nodered", random_string.random.result])
   image = docker_image.nodered_image.image_id
   ports {
     internal = 1880
-    external = 1880
+    # external = 1880
   }
+}
+
+resource "docker_container" "nodered_container2" {
+  name  = join("-",["nodered", random_string.random2.result])
+  image = docker_image.nodered_image.image_id
+  ports {
+    internal = 1880
+    # external = 1880
+  }
+}
+
+output "ip_address" {
+  value = join(":", [docker_container.nodered_container.network_data[0].ip_address, docker_container.nodered_container.ports[0].external])
+  description = "The IP address and port of the container"
+}
+
+output "ip_address2" {
+  value = join(":", [docker_container.nodered_container2.network_data[0].ip_address, docker_container.nodered_container2.ports[0].external])
+  description = "The IP address and port of the container"
+}
+
+output "container-name" {
+  value = docker_container.nodered_container.name
+  description = "The name of the container"
+}
+
+output "container-name2" {
+  value = docker_container.nodered_container2.name
+  description = "The name of the container"
 }
