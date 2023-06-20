8b9425bf5193ea6ee04669deb70ba61ae972509e
Nicholas Lawrence <nlawren@gmail.com>
2023-06-20 13:23:33 +1000

    Update Section 2 - Module 25
    Multiple resouces and count

--- mtc-terraform-docker/main.tf
+++ mtc-terraform-docker/main.tf
@@ -4,10 +4,6 @@ terraform {
       source  = "kreuzwerker/docker"
       version = "~> 3.0.2"
     }
-    random = {
-      source  = "hashicorp/random"
-      version = "~> 3.5.1"
-    }
   }
 }
 
@@ -18,28 +14,15 @@ resource "docker_image" "nodered_image" {
 }
 
 resource "random_string" "random" {
-  length  = 4
+  count = 2
+  length = 4
   special = false
-  upper   = false
-}
-
-resource "random_string" "random2" {
-  length  = 4
-  special = false
-  upper   = false
+  upper = false
 }
 
 resource "docker_container" "nodered_container" {
-  name  = join("-", ["nodered", random_string.random.result])
-  image = docker_image.nodered_image.image_id
-  ports {
-    internal = 1880
-    # external = 1880
-  }
-}
-
-resource "docker_container" "nodered_container2" {
-  name  = join("-", ["nodered", random_string.random2.result])
+  count = 2
+  name  = join("-",["nodered", random_string.random[count.index].result])
   image = docker_image.nodered_image.image_id
   ports {
     internal = 1880
@@ -48,21 +31,21 @@ resource "docker_container" "nodered_container2" {
 }
 
 output "ip_address" {
-  value       = join(":", [docker_container.nodered_container.network_data[0].ip_address, docker_container.nodered_container.ports[0].external])
-  description = "The IP address and port of container one"
+  value = join(":", [docker_container.nodered_container[0].network_data[0].ip_address, docker_container.nodered_container[0].ports[0].external])
+  description = "The IP address and port of the first container"
 }
 
 output "ip_address2" {
-  value       = join(":", [docker_container.nodered_container2.network_data[0].ip_address, docker_container.nodered_container2.ports[0].external])
-  description = "The IP address and port of the container two"
+  value = join(":", [docker_container.nodered_container[1].network_data[0].ip_address, docker_container.nodered_container[1].ports[0].external])
+  description = "The IP address and port of the second container"
 }
 
 output "container-name" {
-  value       = docker_container.nodered_container.name
-  description = "Container One name"
+  value = docker_container.nodered_container[0].name
+  description = "The name of the first container"
 }
 
 output "container-name2" {
-  value       = docker_container.nodered_container2.name
-  description = "Container Two name"
+  value = docker_container.nodered_container[1].name
+  description = "The name of the second container"
 }
