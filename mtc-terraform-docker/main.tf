1d63f4251e87c70260f44b4eddc37c77ec489619
Nicholas Lawrence <nlawren@gmail.com>
2023-06-20 14:51:31 +1000

    MTC Terraform
    Section 2 - Modules 26 and 27
    
    The splat expression and for loops

--- mtc-terraform-docker/main.tf
+++ mtc-terraform-docker/main.tf
@@ -36,21 +36,11 @@ resource "docker_container" "nodered_container" {
 }
 
 output "ip_address" {
-  value       = join(":", [docker_container.nodered_container[0].network_data[0].ip_address, docker_container.nodered_container[0].ports[0].external])
-  description = "The IP address and port of the first container"
-}
-
-output "ip_address2" {
-  value       = join(":", [docker_container.nodered_container[1].network_data[0].ip_address, docker_container.nodered_container[1].ports[0].external])
-  description = "The IP address and port of the second container"
+  value       = [for i in docker_container.nodered_container[*] : join(":", i.network_data[*]["ip_address"], i.ports[*]["external"])]
+  description = "The IP address and port of the containers"
 }
 
 output "container-name" {
-  value       = docker_container.nodered_container[0].name
-  description = "The name of the first container"
-}
-
-output "container-name2" {
-  value       = docker_container.nodered_container[1].name
-  description = "The name of the second container"
+  value       = docker_container.nodered_container[*].name
+  description = "Container names"
 }
