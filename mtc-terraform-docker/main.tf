fd1216c6eb3d77d275123a61a630956330566874
Nicholas Lawrence <nlawren@gmail.com>
2023-06-20 13:36:49 +1000

    ran a terraform fmt

--- mtc-terraform-docker/main.tf
+++ mtc-terraform-docker/main.tf
@@ -19,15 +19,15 @@ resource "docker_image" "nodered_image" {
 }
 
 resource "random_string" "random" {
-  count = 2
-  length = 4
+  count   = 2
+  length  = 4
   special = false
-  upper = false
+  upper   = false
 }
 
 resource "docker_container" "nodered_container" {
   count = 2
-  name  = join("-",["nodered", random_string.random[count.index].result])
+  name  = join("-", ["nodered", random_string.random[count.index].result])
   image = docker_image.nodered_image.image_id
   ports {
     internal = 1880
@@ -36,21 +36,21 @@ resource "docker_container" "nodered_container" {
 }
 
 output "ip_address" {
-  value = join(":", [docker_container.nodered_container[0].network_data[0].ip_address, docker_container.nodered_container[0].ports[0].external])
+  value       = join(":", [docker_container.nodered_container[0].network_data[0].ip_address, docker_container.nodered_container[0].ports[0].external])
   description = "The IP address and port of the first container"
 }
 
 output "ip_address2" {
-  value = join(":", [docker_container.nodered_container[1].network_data[0].ip_address, docker_container.nodered_container[1].ports[0].external])
+  value       = join(":", [docker_container.nodered_container[1].network_data[0].ip_address, docker_container.nodered_container[1].ports[0].external])
   description = "The IP address and port of the second container"
 }
 
 output "container-name" {
-  value = docker_container.nodered_container[0].name
+  value       = docker_container.nodered_container[0].name
   description = "The name of the first container"
 }
 
 output "container-name2" {
-  value = docker_container.nodered_container[1].name
+  value       = docker_container.nodered_container[1].name
   description = "The name of the second container"
 }
