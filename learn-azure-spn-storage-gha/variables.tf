824f8c58ca532b5ffc4338c8b85cfd25725d470b
Nicholas Lawrence <nlawren@gmail.com>
2025-01-03 21:44:39 +1100

    Updating variables for new gha/terraform delivery

---
+++ learn-azure-spn-storage-gha/variables.tf
@@ -0,0 +1,24 @@
+variable "location" {
+  type    = string
+  default = "australiaeast"
+}
+
+variable "naming_prefix" {
+  type    = string
+  default = "isileth"
+}
+
+variable "github_repository" {
+  type    = string
+  default = "github-actions-terraform"
+}
+
+variable "tag_usage" {
+  type    = string
+  default = "terraform-state"
+}
+
+variable "tag_owner" {
+  type    = string
+  default = "nlawren"
+}
