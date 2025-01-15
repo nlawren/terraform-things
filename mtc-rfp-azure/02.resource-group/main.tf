f3a0586cdf08bfe92d8f82575677cc1257675bae
Nicholas Lawrence <nlawren@gmail.com>
2025-01-15 15:28:28 +1100

    Formatting and provider block change

--- mtc-rfp-azure/02.resource-group/main.tf
+++ mtc-rfp-azure/02.resource-group/main.tf
@@ -1,11 +1,11 @@
 terraform {
+  required_version = ">= 1.9"
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
       version = "~> 4.0"
     }
   }
-  required_version = ">= 1.9"
 }
 
 provider "azurerm" {
