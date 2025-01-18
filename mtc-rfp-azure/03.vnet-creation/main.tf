e83959f5443ff8a71476fc5e3bf58063fc3abceb
Nicholas Lawrence <nlawren@gmail.com>
2025-01-18 13:55:03 +1100

    mtc rfp azure module 3 vnet creation

--- mtc-rfp-azure/03.vnet-creation/main.tf
+++ mtc-rfp-azure/03.vnet-creation/main.tf
@@ -1,3 +1,7 @@
+# Links:
+# Azurerm docs: https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs
+# Azurerm vnet docs: https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/virtual_network
+
 terraform {
   required_version = ">= 1.9"
   required_providers {
@@ -9,12 +13,24 @@ terraform {
 }
 
 provider "azurerm" {
+  resource_provider_registrations = "core"
   features {}
 }
 
 resource "azurerm_resource_group" "rfp-rg" {
   name     = "rfp-resources"
   location = "Australia East"
+  tags = {
+    environment = "dev"
+  }
+}
+
+resource "azurerm_virtual_network" "rfp-vnet" {
+  name                = "rfp-network"
+  resource_group_name = azurerm_resource_group.rfp-rg.name
+  location            = azurerm_resource_group.rfp-rg.location
+  address_space       = ["10.0.0.0/16"]
+
   tags = {
     environment = "dev"
   }
