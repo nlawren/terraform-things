371adaaa8758ff125fa4e31658b152634843c3d5
Nicholas Lawrence <nlawren@gmail.com>
2025-02-20 14:11:20 +1100

    Resolving linter errors. Removed domain and  account_id - passed in via env variables. Added type = string to zone_id

--- mtc-rfp-azure/10.custom.data/variables.tf
+++ mtc-rfp-azure/10.custom.data/variables.tf
@@ -4,12 +4,5 @@ provider "cloudflare" {
 
 variable "zone_id" {
   default = "<YOUR_ZONE_ID>"
-}
-
-variable "account_id" {
-  default = "<YOUR_ACCOUNT_ID>"
-}
-
-variable "domain" {
-  default = "<YOUR_DOMAIN>"
+  type = string
 }
