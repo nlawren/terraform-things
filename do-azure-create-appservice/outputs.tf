453a6c9d3ec6568544ad9f5b4ae2a33830f55c5f
Nicholas Lawrence <nlawren@gmail.com>
2025-06-21 08:31:33 +1000

    Add the web app name (needed for the ADO yaml pipeline) to the output

--- do-azure-create-appservice/outputs.tf
+++ do-azure-create-appservice/outputs.tf
@@ -6,6 +6,10 @@ output "service_plan_name" {
   value = azurerm_service_plan.asp.name
 }
 
+output "linux_web_app_name" {
+  value = azurerm_linux_web_app.webapp.name
+}
+
 output "linux_web_app_hostname" {
   value = azurerm_linux_web_app.webapp.default_hostname
 }
